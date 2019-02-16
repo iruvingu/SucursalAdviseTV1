4f6845f882e37b4a74a0e5d4d5d00b648d18cc64
Irving Flores <rfloresg1002@alumno.ipn.mx>
2019-02-15 19:06:24 -0600

    [MODIFIED] Layouts and Date were modified

--- app/src/main/java/com/example/sucursaladvisetv1/HeadScreenFragment.java
+++ app/src/main/java/com/example/sucursaladvisetv1/HeadScreenFragment.java
@@ -15,29 +15,23 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HeadScreenFragment extends Fragment {
-    private static final String CERO = "0";
-    private static final String BARRA = "/";
-    private TimePicker timePicker1;
+
+    private TextView dateTextView;
 
     //Calendario para obtener fecha & hora
-    public final Calendar c = Calendar.getInstance();
-
-    //Variables para obtener la fecha
-    final int mes = c.get(Calendar.MONTH);
-    final int dia = c.get(Calendar.DAY_OF_MONTH);
-    final int anio = c.get(Calendar.YEAR);
-
-    //Widgets
-    EditText etFecha;
-    ImageButton ibObtenerFecha;
+    public final Date currentTime = Calendar.getInstance().getTime();
 
     public HeadScreenFragment() {
         // Required empty public constructor
@@ -51,41 +45,13 @@ public class HeadScreenFragment extends Fragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_head_screen, container, false);
 
-        //Widget EditText donde se mostrara la fecha obtenida
-        etFecha = (EditText) root.findViewById(R.id.et_mostrar_fecha_picker);
-        timePicker1 = (TimePicker) root.findViewById(R.id.timePicker1);
-        obtenerFecha();
-
-        int hour = timePicker1.getCurrentHour();
-        int min = timePicker1.getCurrentMinute();
+        dateTextView =  root.findViewById(R.id.dateView);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm a");
+        String currentDateandTime = sdf.format(currentTime);
+        dateTextView.setText(currentDateandTime);
 
         return root;
     }
 
-    private void obtenerFecha() {
-
-        DatePickerDialog recogerFecha = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                //Esta variable lo que realiza es aumentar en uno el mes ya que comienza desde 0 = enero
-                final int mesActual = month + 1;
-                //Formateo el día obtenido: antepone el 0 si son menores de 10
-                String diaFormateado = (dayOfMonth < 10)? CERO + String.valueOf(dayOfMonth):String.valueOf(dayOfMonth);
-                //Formateo el mes obtenido: antepone el 0 si son menores de 10
-                String mesFormateado = (mesActual < 10)? CERO + String.valueOf(mesActual):String.valueOf(mesActual);
-                //Muestro la fecha con el formato deseado
-                etFecha.setText(diaFormateado + BARRA + mesFormateado + BARRA + year);
-
-
-            }
-            //Estos valores deben ir en ese orden, de lo contrario no mostrara la fecha actual
-            /**
-             *También puede cargar los valores que usted desee
-             */
-        },anio, mes, dia);
-        //Muestro el widget
-        recogerFecha.show();
-
-    }
 
 }
