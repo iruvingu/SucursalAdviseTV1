bca08c17370c7b54fba3dc3afe7352bcb10ae165
Luis Alberto Dominguez Borjas <ing.roboticaluisd@gmail.com>
2019-03-06 12:56:29 -0600

    [ADD] Android Device ID
    Obtiene el serial number

--- app/src/main/java/com/example/sucursaladvisetv1/MainActivity.java
+++ app/src/main/java/com/example/sucursaladvisetv1/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.sucursaladvisetv1;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String androidDeviceId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        androidDeviceId = Settings.Secure.getString(getApplicationContext().getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+        Log.v("Etiqueta de Id Divece", "Android ID: " + androidDeviceId);
     }
 }
