8b2801ac82912ed9e1f34ecaf5c23b31c33412fc
Irving Flores <rfloresg1002@alumno.ipn.mx>
2019-03-12 19:15:15 -0600

    [MODIFIED] XML FragmentVideo in the project was modified

--- app/src/main/java/com/example/sucursaladvisetv/MainCentralFragment.java
+++ app/src/main/java/com/example/sucursaladvisetv/MainCentralFragment.java
@@ -181,6 +181,7 @@ public class MainCentralFragment extends Fragment {
                                     stopRunnable();
                                 }  else {
                                     startRunnable();
+                                    //XD
                                 }
                             }
 
